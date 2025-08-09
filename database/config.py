96b2510b5aac4ccf42e3d092645a95510343315a
maksEveline <maksimeveline@gmail.com>
2025-08-09 21:25:40 +0300

    changed token

--- database/config.py
+++ database/config.py
@@ -6,4 +6,4 @@ DEPOSIT_REG = "database/deposit_regular.txt"
 TRASH_REG = "database/trash_regular.txt"
 
 DEEPSEEK_API = "sk-0eaaf2ad59d547939fafd9953f03e83e"
-BOT_TOKEN = "7682380679:AAFdq2roYQgRQX9XJu6fNpmH-sBVG2MMW0A"
+BOT_TOKEN = "8193127329:AAFdctmZkETZMcg6d6-jrGMO8hBPDqaGbVs"
